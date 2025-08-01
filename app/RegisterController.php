d7b434f294c499261683c057ecd223cef25a7c62
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-08-01 18:16:57 +0530

    Tempestphp App

---
+++ app/RegisterController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Tempest\Router\Get;
+use Tempest\View\View;
+
+use function Tempest\view;
+
+final class RegisterController
+{
+    #[Get(uri: '/register')]
+    public function __invoke(): View
+    {
+        return view('register.view.php');
+    }
+}
